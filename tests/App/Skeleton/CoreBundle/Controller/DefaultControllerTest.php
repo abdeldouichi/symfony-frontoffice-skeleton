25fd03d9b10ad7d11a1d3439323ad225c2c6eb8b
abdeldouichi <abdel.douichi@gmail.com>
2020-07-26 18:28:34 +0200

    initial skeleton

---
+++ tests/App/Skeleton/CoreBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Skeleton\CoreBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
